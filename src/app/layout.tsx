944785cf2ec88a81e025404a46bdf10570cd4d25
Muhammad Sufyan <computer388unofficial@gmail.com>
2024-11-07 14:27:28 +0500

    navbar added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Michroma, Montserrat } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
+const michroma = Michroma({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-michroma",
+});
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-montserrat",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +27,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body
+        className={`${inter.className} ${montserrat.variable} ${michroma.variable}`}
+      >
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ src/components/header.tsx
@@ -0,0 +1,126 @@
+"use client";
+
+import { Container } from "@mui/material";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import logo from "../../public/Techticslogo.svg";
+import Link from "next/link";
+
+function Header() {
+  const [isscroll, setisscroll] = useState(false);
+  const [clicked, setclicked] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 60) {
+        setisscroll(true);
+      } else {
+        setisscroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    // Clean up the event listener on component unmount
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div
+      className={`h-24 flex items-center sticky inset-0 cursor-pointer border ${
+        clicked &&
+        "!items-start min-h-screen py-6 bg-gradient-to-b from-[#060A30] to-[#0416FE]"
+      }  ${isscroll ? "bg-inherit" : "bg-transparent"}`}
+      onMouseEnter={() => setisscroll(true)}
+      onMouseLeave={() => {
+        if (window.scrollY < 60) {
+          setisscroll(false);
+        }
+      }}
+      style={{
+        transition: "background-color 0.7s",
+      }}
+    >
+      <Container maxWidth="lg">
+        <div className="flex justify-between items-center h-full">
+          <div className="w-[200px] md:w-[300px] h-[49px] relative">
+            <Image
+              src={logo}
+              alt="Logo"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              className="object-center object-contain"
+            />
+          </div>
+          <div className="font-Montserrat font-semibold text-base text-white uppercase  gap-6 justify-center items-center flex-grow tracking-widest lg:flex hidden">
+            <Link
+              href="/"
+              className="hover:text-[#45E7FF] transition-colors duration-500"
+            >
+              About us
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#45E7FF] transition-colors duration-500"
+            >
+              Services
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#45E7FF] transition-colors duration-500"
+            >
+              Portfolio
+            </Link>
+            <Link
+              href="/"
+              className="hover:text-[#45E7FF] transition-colors duration-500"
+            >
+              Blogs
+            </Link>
+          </div>
+          <button className="font-Montserrat font-semibold text-base text-black bg-gradient-to-r from-[#FEFC45] to-[#41FE51] text-center w-[215px] h-[50px] rounded-md uppercase tracking-widest hidden lg:block hover:from-white hover:to-white ">
+            Consult us
+          </button>
+          <div className="block lg:hidden relative md:w-14 md:h-14 w-10 h-10">
+            {clicked ? (
+              <Image
+                src="/closeicon.png"
+                alt="menubar"
+                fill
+                onClick={() => setclicked((pre) => !pre)}
+              />
+            ) : (
+              <Image
+                src="/menubar.png"
+                alt="menubar"
+                fill
+                onClick={() => setclicked((pre) => !pre)}
+              />
+            )}
+          </div>
+
+          {/* {clicked && (
+            <div className="fixed left-0 top-[96px] h-screen w-full border lg:hidden flex  justify-center bg-red-500">
+              <div className="text-white font-bold text-4xl  ">Hello world</div>
+            </div>
+          )} */}
+        </div>
+        {clicked && (
+          <div className="flex flex-col gap-6 font-Montserrat text-white text-xl tracking-widest font-extrabold items-center justify-center mt-20 md:mt-28 md:text-2xl lg:hidden">
+            <Link href="/" className="uppercase hover:text-[#45E7FF]">
+              About us
+            </Link>
+            <Link href="/" className="uppercase hover:text-[#45E7FF]">
+              Services
+            </Link>
+            <Link href="/" className="uppercase hover:text-[#45E7FF]">
+              Portfolio
+            </Link>
+            <Link href="/" className="uppercase hover:text-[#45E7FF]">
+              Blogs
+            </Link>
+          </div>
+        )}
+      </Container>
+    </div>
+  );
+}
+
+export default Header;
